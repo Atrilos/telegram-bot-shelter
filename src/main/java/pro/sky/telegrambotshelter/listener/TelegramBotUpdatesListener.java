4967d9d488a8d46188fae8c319d7a82af28984a7
Atrilos <101889118+Atrilos@users.noreply.github.com>
2023-01-14 18:11:15 +0700

    init

---
+++ src/main/java/pro/sky/telegrambotshelter/listener/TelegramBotUpdatesListener.java
@@ -0,0 +1,34 @@
+package pro.sky.telegrambotshelter.listener;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class TelegramBotUpdatesListener implements UpdatesListener {
+
+    private final TelegramBot telegramBot;
+
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);
+    }
+
+    @Override
+    public int process(List<Update> updates) {
+        updates.forEach(update -> {
+            log.info("Processing update: {}", update);
+            // Process your updates here
+        });
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+}
