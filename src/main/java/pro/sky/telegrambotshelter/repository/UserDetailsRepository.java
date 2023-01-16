12b6dd94e027bbc4c1a6bf60e85064aa75af47f9
Atrilos <101889118+Atrilos@users.noreply.github.com>
2023-01-16 23:02:58 +0700

    Add repositories

---
+++ src/main/java/pro/sky/telegrambotshelter/repository/UserDetailsRepository.java
@@ -0,0 +1,9 @@
+package pro.sky.telegrambotshelter.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pro.sky.telegrambotshelter.model.UserDetails;
+
+@Repository
+public interface UserDetailsRepository extends JpaRepository<UserDetails, Long> {
+}
