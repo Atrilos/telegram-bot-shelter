8f7b2254cc25aa42c0b55ab070e661a48f19ccd3
Vasily Klementev <101889118+Atrilos@users.noreply.github.com>
2023-01-23 19:01:18 +0700

    Revert "JavaDocs for stage 1"

--- src/main/java/pro/sky/telegrambotshelter/repository/ShelterDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package pro.sky.telegrambotshelter.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pro.sky.telegrambotshelter.model.ShelterDetails;
-
-@Repository
-public interface ShelterDetailsRepository extends JpaRepository<ShelterDetails, Long> {
-}
