2c69ddb6dcf0e49436bbbe5173a99bfeaef95a70
Atrilos <101889118+Atrilos@users.noreply.github.com>
2023-02-09 05:16:26 +0700

    Setup for TestContainer's UserRepository tests

---
+++ src/test/java/pro/sky/telegrambotshelter/repository/UserRepositoryTest.java
@@ -0,0 +1,47 @@
+package pro.sky.telegrambotshelter.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import pro.sky.telegrambotshelter.model.User;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryTest {
+
+    @Container
+    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:alpine"));
+    @Autowired
+    private UserRepository out;
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url=", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username=", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password=", postgreSQLContainer::getPassword);
+    }
+
+    @Test
+    public void findByChatId() {
+        User a = User.builder().chatId(1L).firstName("A").build();
+
+        assertThat(out.findByChatId(1L)).isEqualTo(Optional.empty());
+        entityManager.persist(a);
+        assertThat(out.findByChatId(1L)).isEqualTo(Optional.of(a)); //equality by natural id, so no need to check id
+    }
+}
